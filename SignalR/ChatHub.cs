3deff8218b4a34be6a16b0e25cc4e8bd1ba60ec7
Alexander Nygaard <1067406@ucn.dk>
2018-09-17 09:56:52 +0200

    Small change

--- SignalR/ChatHub.cs
+++ SignalR/ChatHub.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SignalR {
     public class ChatHub : Hub {
-
+        // The hubs task is to just throw back the messages to all the conncted users
         public async Task Send(string name, string message) {
             await Clients.All.SendAsync("Send", name, message);
         }
